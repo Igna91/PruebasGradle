170499271da3fac2df3ed48a0cb4326b2b388733
Virginia <virginia.morte@gmail.com>
2016-02-15 22:58:45 +0100

    librerias para cucumber

---
+++ app/src/androidTest/java/com/ps/grupo2/comunio/CucumberTestCase.java
@@ -0,0 +1,12 @@
+package com.ps.grupo2.comunio;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(
+        //this code will only look into "features/" folder for features
+        features={"classpath:features/"},
+        glue = { "com.mycompany.cucumber.stepdefinitions", "com.mycompany.cucumber.hooks" },
+        format = { "com.mycompany.cucumber.formatter.RuntimeInfoCatcher", "json:target/cucumber.json" },
+        tags = { "@working" }
+)
+public class CucumberStarterIT {
+}
